17bf20acb8473d91cd1b3452d6882350c1be7fe0
ADITYAARYAN-01 <adityaaryan174@gmail.com>
2025-12-13 02:00:14 +0530

    ADDED CLIENT SIDE VALIDATION VIA BOOTSTRAP

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Listing = require("./MODELS/listing.js");
 const path = require("path");
-const methodOverride  = require("method-override");
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
 const port = 8080;
@@ -12,12 +12,12 @@ app.set("views", path.join(__dirname, "views"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"));
-app.engine('ejs',ejsMate);
+app.engine('ejs', ejsMate);
 
-app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(path.join(__dirname, "/public")))
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("HI I AM ROOT")
 })
 
@@ -34,65 +34,75 @@ app.get("/",(req,res)=>{
 //     console.log("SAMPLE WAS SAVED");
 //     res.send("successful testing")
 // })
- 
 
-app.get("/listings", async (req,res) =>{
+
+app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index", { allListings });
 })
 
 //New Routes
-app.get("/listings/new", async (req,res) =>{
+app.get("/listings/new", async (req, res) => {
     res.render("listings/new");
 })
 
 // SHOW ROUTE
-app.get("/listings/:id" , async(req,res) =>{
-    let {id}= req.params;
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/show" , {listing})
+    res.render("listings/show", { listing })
 })
 
 //CREATE ROUTE 
-app.post("/listings" , async(req,res) =>{
-    let listingnew = new Listing(req.body.listing);
-    await listingnew.save();
-    res.redirect("/listings")
+app.post("/listings", async (req, res, next) => {
+    try {
+        let listingnew = new Listing(req.body.listing);
+        await listingnew.save();
+        res.redirect("/listings")
+    }
+    catch(err){
+        next(err);
+    }
 })
 
 //EDIT ROUTE
 app.get("/listings/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit", { listing }); 
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit", { listing });
 });
 
 //UPDATE ROUTE
-app.put("/listings/:id", async(req, res) =>{
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id , {...req.body.listing})
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing })
     res.redirect(`/listings/${id}`)
 })
- //DELETE ROUTE
+//DELETE ROUTE
 
- app.delete("/listings/:id" , async(req,res) =>{
-    let {id} = req.params;
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
     let deleted_listing = await Listing.findByIdAndDelete(id);
     console.log(deleted_listing)
     res.redirect("/listings");
- })
+})
+
+
+app.use((err, req, res, next) => {
+    res.send("SomeThing went wrong") 
+})
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/WonderLustMain');
 }
 main()
-    .then( c => {
-       console.log("Connected Successfully");
+    .then(c => {
+        console.log("Connected Successfully");
     })
-    .catch((err) =>{
+    .catch((err) => {
         console.log(err);
     })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`SERVER IS LISTENING ON PORT ${port}`)
 })
