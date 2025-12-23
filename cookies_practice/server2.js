3a13a8c051f6b6fcb5570dd0b0c9620a0eb6b96e
ADITYAARYAN-01 <adityaaryan174@gmail.com>
2025-12-23 22:52:26 +0530

    added CONTENT FLASH concepts

--- cookies_practice/server2.js
+++ cookies_practice/server2.js
@@ -4,20 +4,39 @@ const app = express();
 const port = 3000;
 const cookieparser = require("cookie-parser")
 const session = require("express-session")
+const flash = require("connect-flash")
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
 
+const sessionOptions = {
+    secret: "mysupersecretstring",
+    resave: false,
+    saveUninitialized: true,
+}
 app.use(
-    session({
-        secret: "mysupersecretstring",
-        resave: false,
-        saveUninitialized: true,
-    })
+    session(sessionOptions)
 );
+app.use(flash())
+
+app.get("/register", (req, res) => {
+    let { name = "anonymous" } = req.query;
+    req.session.name = name;
+    // res.send(name);
+    req.flash("success", "USER REGISTERED SUCCESSFULLY")
+    res.redirect("/hello")
+});
+
+app.get("/hello", (req, res) => {
+    console.log(req.flash('success'));
+    res.render("page.ejs", { name: req.session.name, msg: req.flash("success") })
+})
 
 app.get("/reqcount", (req, res) => {
-    if(req.session.count){
+    if (req.session.count) {
         req.session.count++;
     }
-    else{
+    else {
         req.session.count = 1;
     }
     res.send(`You have sent a request  ${req.session.count} times`)
