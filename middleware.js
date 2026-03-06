be89716f55b5ec83ee80023537dbd8d8d5b4fb17
ADITYAARYAN-01 <adityaaryan174@gmail.com>
2026-03-07 00:35:05 +0530

    automatic login after signup

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req,res,next) =>{
+    console.log(req.user);
     if(!req.isAuthenticated()){
         req.flash("error","YOU MUST BE LOGGED IN TO CREATE LISTING");
         return res.redirect("/login");
