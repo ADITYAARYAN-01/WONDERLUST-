7585fd5fc2d59d45a918ee7620fc44c624c95ff8
ADITYAARYAN-01 <adityaaryan174@gmail.com>
2026-03-17 17:49:32 +0530

    implemented mvc on user.js

--- routes/user.js
+++ routes/user.js
@@ -3,26 +3,29 @@ const router = express.Router();
 const User = require("../MODELS/user.js");
 const wrapAsync = require('../utils/wrapAsync');
 const passport = require('passport');
-const {savedRedirectUrl} = require("../middleware.js")
+const { savedRedirectUrl } = require("../middleware.js")
 const userController = require("../controllers/user.js")
 
 
-router.get("/signup",userController.renderSignupForm)
+router.route("/signup")
+    .get(userController.renderSignupForm)
+    .post(wrapAsync(userController.signup))
 
 
-router.post("/signup", wrapAsync(userController.signup))
+// router.get("/signup", userController.renderSignupForm)
+// router.post("/signup", wrapAsync(userController.signup))
 
 // for login
 router.get("/login", userController.renderLoginForm)
 
 router.post(
-    "/login",savedRedirectUrl, passport.authenticate("local",
-        {
+    "/login", savedRedirectUrl, passport.authenticate("local",
+        { 
             failureRedirect: "/login", failureFlash: true
 
         }),
-        userController.login
-    )
+    userController.login
+)
 
 //logout
 
